5693c40a7860c535b43b684714118a0b932fe1e6
Iuliia Ashikhmina <yu.ashikhmina@gmail.com>
2018-11-04 21:09:10 +0100

    initial setup

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/Aux'
+
+class BurgerBuider extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+
+        )
+    }
+}
+
+export default BurgerBuider
